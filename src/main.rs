a6ac1016597c848444d934b9f834eb6cdbdf618d
indierusty <priyaanyadav@gmail.com>
2024-12-22 15:55:48 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
+use std::fs::OpenOptions;
 use std::io::{self, Write};
-use std::process::Command;
+use std::process::{Command, Output};
 use std::{env, fs};
 
 const BUILTIN_CMDS: [&str; 5] = ["cd", "echo", "exit", "type", "pwd"];
@@ -94,6 +95,19 @@ fn parse(src: &str) -> Vec<String> {
     cmds
 }
 
+#[derive(Debug)]
+enum RedirectionMode {
+    Default,
+    Append(String),
+    Direct(String),
+}
+
+#[derive(Debug)]
+enum Channel {
+    Stdout,
+    Stderr,
+}
+
 fn main() -> anyhow::Result<()> {
     let stdin = io::stdin();
     let mut input = String::new();
@@ -106,8 +120,51 @@ fn main() -> anyhow::Result<()> {
         stdin.read_line(&mut input).unwrap();
 
         let mut exit_status = 0;
-        let mut i = 0;
+        let mut stdout_redirection = RedirectionMode::Default;
+        let mut stderr_redirection = RedirectionMode::Default;
+
         let cmds = parse(&input);
+        let mut new_cmds: Vec<String> = Vec::new();
+
+        let mut i = 0;
+        new_cmds.push(cmds[i].clone());
+        i += 1;
+
+        while i < cmds.len() {
+            if [">", "1>", ">>", "1>>"].contains(&cmds[i].as_str()) {
+                i += 1;
+                if i < cmds.len() {
+                    let mode = cmds[i - 1].as_str();
+                    let path = cmds[i].clone();
+                    stdout_redirection = if mode == ">" || mode == "1>" {
+                        RedirectionMode::Direct(path)
+                    } else {
+                        RedirectionMode::Append(path)
+                    };
+                } else {
+                    eprint!("ERR: path is not given for output redirection\n")
+                }
+            } else if ["2>", "2>>"].contains(&cmds[i].as_str()) {
+                i += 1;
+                if i < cmds.len() {
+                    let mode = cmds[i - 1].as_str();
+                    let path = cmds[i].clone();
+                    stderr_redirection = if mode == "2>>" {
+                        RedirectionMode::Append(path)
+                    } else {
+                        RedirectionMode::Direct(path)
+                    };
+                } else {
+                    eprint!("ERR: path is not given for output redirection\n")
+                }
+            } else {
+                new_cmds.push(cmds[i].clone());
+            }
+            i += 1;
+        }
+
+        let cmds = new_cmds;
+        let mut i = 0;
 
         if i < cmds.len() {
             let cmd = &cmds[i];
@@ -119,10 +176,15 @@ fn main() -> anyhow::Result<()> {
                         let key = "HOME";
                         if let Some(home_path) = env::var_os(key) {
                             if let Err(_) = env::set_current_dir(&home_path) {
-                                print!(
-                                    "cd: {}: No such file or directory\n",
-                                    home_path.into_string().unwrap()
-                                )
+                                write(
+                                    &stderr_redirection,
+                                    Channel::Stderr,
+                                    format!(
+                                        "cd: {}: No such file or directory\n",
+                                        home_path.into_string().unwrap()
+                                    )
+                                    .as_bytes(),
+                                );
                             }
                         }
                     }
@@ -130,7 +192,12 @@ fn main() -> anyhow::Result<()> {
                         let path = &cmds[i];
                         i += 1;
                         if let Err(_) = env::set_current_dir(path) {
-                            print!("cd: {}: No such file or directory\n", path)
+                            // print!("cd: {}: No such file or directory\n", path);
+                            write(
+                                &stderr_redirection,
+                                Channel::Stderr,
+                                format!("cd: {}: No such file or directory\n", path).as_bytes(),
+                            );
                         }
                         _ = i; // NOTE: just to remove unused assignment warning
                     }
@@ -138,38 +205,53 @@ fn main() -> anyhow::Result<()> {
                 "pwd" => {
                     let pwd = env::current_dir()?;
                     let pwd = pwd.to_str().unwrap();
-                    print!("{}\n", pwd);
+                    write(
+                        &stdout_redirection,
+                        Channel::Stdout,
+                        format!("{}\n", pwd).as_bytes(),
+                    );
+                    // print!("{}\n", pwd);
                 }
                 "type" => {
                     if i < cmds.len() {
                         let cmd = &cmds[i];
+
                         i += 1;
                         _ = i;
+
+                        let res;
+
                         if BUILTIN_CMDS.contains(&cmd.as_str()) {
-                            print!("{} is a shell builtin\n", cmd);
+                            res = Some(format!("{} is a shell builtin\n", cmd));
                         } else {
                             let path_cmds = path_cmds()?;
                             if path_cmds.contains_key(cmd.as_str()) {
                                 let path = &path_cmds[cmd];
-                                print!("{} is {}/{}\n", cmd, path, cmd)
+                                res = Some(format!("{} is {}/{}\n", cmd, path, cmd));
                             } else {
-                                print!("{}: not found\n", cmd);
+                                res = Some(format!("{}: not found\n", cmd));
                             }
+                        }
+
+                        if let Some(res) = res {
+                            write(&stdout_redirection, Channel::Stdout, res.as_bytes());
                         }
                     }
                 }
                 "echo" => {
+                    let mut res = String::new();
                     loop {
                         if i == cmds.len() {
                             break;
                         }
-                        print!("{}", cmds[i]);
+                        res.push_str(cmds[i].as_str());
                         if i != cmds.len() - 1 {
-                            print!(" ");
+                            res.push(' ');
                         }
                         i += 1;
                     }
-                    print!("\n");
+                    res.push('\n');
+                    write(&stdout_redirection, Channel::Stdout, res.as_bytes());
                 }
                 "exit" => {
                     if i < cmds.len() {
@@ -183,17 +265,44 @@ fn main() -> anyhow::Result<()> {
                     let path_cmds = path_cmds()?;
                     if path_cmds.contains_key(cmd) {
                         let path = format!("{}/{}", path_cmds[cmd], cmd);
-                        Command::new(path)
+                        let Output {
+                            status: _,
+                            stdout,
+                            stderr,
+                        } = Command::new(cmd)
                             .args(&cmds[i..])
-                            .status()
+                            .output()
                             .expect("failed to execute process");
+
+                        write(&stdout_redirection, Channel::Stdout, &stdout);
+                        write(&stderr_redirection, Channel::Stderr, &stderr);
                     } else {
-                        print!("{}: command not found\n", cmd.trim_end());
+                        let res = format!("{}: command not found\n", cmd.trim_end());
+                        write(&stderr_redirection, Channel::Stderr, res.as_bytes());
                     }
                 }
             }
         }
         io::stdout().flush().unwrap();
+    }
+}
+
+fn write(mode: &RedirectionMode, std_type: Channel, src: &[u8]) {
+    match mode {
+        RedirectionMode::Default => match std_type {
+            Channel::Stdout => io::stdout().write_all(src).unwrap(),
+            Channel::Stderr => io::stderr().write_all(src).unwrap(),
+        },
+        RedirectionMode::Append(path) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(path)
+                .unwrap();
+
+            file.write_all(&src).unwrap();
+        }
+        RedirectionMode::Direct(path) => fs::write(path, src).unwrap(),
     }
 }
 
